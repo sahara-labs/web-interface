3e4d8f14fa3ae8efb3b68aeeca4e86e431051c17
mdiponio <mdiponio@7ee224fa-57ca-4419-87f9-1f4314938d77>
2010-06-19 01:14:29 +0000

    Source styling.
    git-svn-id: https://develop.eng.uts.edu.au/projects/personal/sahara/svn/WI/trunk@1069 7ee224fa-57ca-4419-87f9-1f4314938d77

--- public/index.php
+++ public/index.php
@@ -36,27 +36,26 @@
  * @date 17th March 2010
  */
 
-/* Define path to application directory */
+/*---- Define path to application directory ---------------------------------*/
 defined('APPLICATION_PATH')
     || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
 
-/* Define application environment */
+/*---- Define application environment ---------------------------------------*/
 defined('APPLICATION_ENV')
     || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
 
-/* Ensure library/ is on include_path. */
+/*---- Put code folders on include_path. ------------------------------------*/
 set_include_path(implode(PATH_SEPARATOR, array(
-    realpath(APPLICATION_PATH . '/../library'),
-    realpath(APPLICATION_PATH . '/models'),
-    realpath(APPLICATION_PATH . '/../institution'),
+    realpath(APPLICATION_PATH . '/../library'),     // Library code, Zend & Sahara_*
+    realpath(APPLICATION_PATH . '/models'),         // Model code
+    realpath(APPLICATION_PATH . '/../institution'), // Institution code 
     get_include_path()
 )));
 
-/* Zend_Application */
 require_once 'Zend/Application.php';
 
-/* Create application, bootstrap, and run. */
+/*---- Create application, bootstrap, and run. ------------------------------*/
 $application = new Zend_Application(APPLICATION_ENV, APPLICATION_PATH . '/configs/application.ini');
-
 $application->bootstrap()
             ->run();
+
