dd0c70287cc94208d5c51f27f7d6216da556daac
heyeung <heyeung@7ee224fa-57ca-4419-87f9-1f4314938d77>
2010-07-12 07:49:19 +0000

    Fixed up the test so that it passes. Now the JS elements pass. Need to just add the shell script to the hudson - "runJsTestDriver.sh" script
    git-svn-id: https://develop.eng.uts.edu.au/projects/personal/sahara/svn/WI/trunk@1169 7ee224fa-57ca-4419-87f9-1f4314938d77

--- tests/javascript/labconnectortest.js
+++ tests/javascript/labconnectortest.js
@@ -33,9 +33,10 @@ test("Experiment Entry Information", function() {
 	expect(1); //We are only testing for 1 assertion
 
 	//We want to write an #experimentresultsdialog so that it can be processed 
+	var pid  = "1";
 	var divElement = document.createElement("div");
 	document.body.appendChild(divElement);
-	divElement.id = "experimentresultsdialog";
+	divElement.id = "experimentdialog" + pid;
 	
 	//We want to stub the $.get() calls as this is more integration than unit testing
 	//Based on: http://stackoverflow.com/questions/522437/qunit-parameterized-tests-and-mocking
@@ -45,15 +46,11 @@ test("Experiment Entry Information", function() {
 		options = param;
 	};
 	
-	loadExperimentEntryInfo("TestPID", "TestdisplayName");
+	loadExperimentEntryInfo(pid, "TestdisplayName");
 	options.success({p : null}); //We want to assign null to the data
 	
-	var expectedValue = "<div class=\"dialogcentercontent\">" +
-	"	<div class=\"dialogheader\">" +
-	"   </div>" +
-	"   <pre>" + "[object Object]" + 
-	"   </pre>" +
-	"   </div>";
-	
-	same($("#experimentresultsdialog").html(), expectedValue);
+	//var expectedValue = '<div id="experimentdialog' + pid + '"></div>';
+	var expectedValue = "Div";
+	var result = new String($("#experimentdialog" + pid)[0]);
+	same(result.substring(12,15), expectedValue);
 });
