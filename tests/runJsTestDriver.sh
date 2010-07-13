88dca965b5dbdd44514149dc0bbc52411d8fe902
heyeung <heyeung@7ee224fa-57ca-4419-87f9-1f4314938d77>
2010-07-13 02:50:50 +0000

    Removing the debugging option. Not needed.
    git-svn-id: https://develop.eng.uts.edu.au/projects/personal/sahara/svn/WI/trunk@1185 7ee224fa-57ca-4419-87f9-1f4314938d77

--- tests/runJsTestDriver.sh
+++ tests/runJsTestDriver.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -jar library/JsTestDriver-1.2.2.jar --port 9876 --config jsTestDriver.conf --browser firefox --tests all --testOutput coverage --runnermode DEBUG
+java -jar library/JsTestDriver-1.2.2.jar --port 9876 --config jsTestDriver.conf --browser firefox --tests all --testOutput coverage 
