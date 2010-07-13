8fd36e8257e62547dcec10b5659b6318dfa591ef
mdiponio <mdiponio@7ee224fa-57ca-4419-87f9-1f4314938d77>
2010-07-13 06:59:49 +0000

    Fixed ci error.
    git-svn-id: https://develop.eng.uts.edu.au/projects/personal/sahara/svn/WI/trunk@1202 7ee224fa-57ca-4419-87f9-1f4314938d77

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -62,13 +62,9 @@ class IndexController extends Sahara_Controller_Action_Acl
             $username = $form->getValue('username');
             $password = $form->getValue('password');
 
-            $auth = new Sahara_Auth($username, $password);
-            if (!$auth->authenticate())
-            {
-                 $this->view->messages = array("Authentication failure.");
-                 return;
-            }
-            $auth->setupSession();
+            /******************************************************************
+             ** TODO Add your authentication.                                **
+             ******************************************************************/
 
             /* Store the authentication information. */
             $qName =  $inst . ':' . $username;
@@ -76,7 +72,14 @@ class IndexController extends Sahara_Controller_Action_Acl
             $storage->clear();
             $storage->write($qName);
 
+
             $user = Sahara_Soap::getSchedServerPermissionsClient()->getUser(array('userQName' => $qName));
+            if ($user->persona == 'NOTFOUND')
+            {
+                /**************************************************************
+                 ** TODO Try to find about the user and add permissions.     **
+                 **************************************************************/
+            }
 
             /* Redirect to an appropriate page. */
             switch ($user->persona)
