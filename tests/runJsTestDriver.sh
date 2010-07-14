5c2afc7886a39211fa214272613000c4b2f9b38e
heyeung <heyeung@7ee224fa-57ca-4419-87f9-1f4314938d77>
2010-07-14 00:40:28 +0000

    Changing port number again..
    git-svn-id: https://develop.eng.uts.edu.au/projects/personal/sahara/svn/WI/trunk@1212 7ee224fa-57ca-4419-87f9-1f4314938d77

--- tests/runJsTestDriver.sh
+++ tests/runJsTestDriver.sh
@@ -21,7 +21,7 @@ PID_XVFB="$!"      # take the process ID
 export DISPLAY=:99 # set display to use that of the xvfb
 
 # run the tests
-java -jar library/JsTestDriver-1.2.2.jar --config jsTestDriver.conf --port 4424 --browser $FIREFOX --tests all --testOutput $OUTPUT_DIR
+java -jar library/JsTestDriver-1.2.2.jar --config jsTestDriver.conf --port 4224 --browser $FIREFOX --tests all --testOutput $OUTPUT_DIR
 
 kill $PID_XVFB     # shut down xvfb (firefox will shut down cleanly by JsTestDriver)
 echo "Done."
