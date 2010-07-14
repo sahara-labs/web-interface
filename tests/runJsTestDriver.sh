c82e376a4e6ebfce4f64a333ac0ef08436bd54b5
heyeung <heyeung@7ee224fa-57ca-4419-87f9-1f4314938d77>
2010-07-14 00:12:34 +0000

    Adding extra calls as a framebuffer needs to be called in order to invoke the web browser.
    git-svn-id: https://develop.eng.uts.edu.au/projects/personal/sahara/svn/WI/trunk@1208 7ee224fa-57ca-4419-87f9-1f4314938d77

--- tests/runJsTestDriver.sh
+++ tests/runJsTestDriver.sh
@@ -1,2 +1,28 @@
-#!/bin/sh
-java -jar library/JsTestDriver-1.2.2.jar --port 9876 --config jsTestDriver.conf --browser firefox --tests all --testOutput coverage 
+#!/bin/bash
+# directory to write output XML (if this doesn't exist, the results will not be generated!)
+OUTPUT_DIR="coverage"
+mkdir $OUTPUT_DIR
+
+XVFB=`which Xvfb`
+if [ "$?" -eq 1 ];
+then
+    echo "Xvfb not found."
+    exit 1
+fi
+
+FIREFOX=`which firefox`
+if [ "$?" -eq 1 ];
+then
+    echo "Firefox not found."
+    exit 1
+fi
+
+$XVFB :99 -ac &    # launch virtual framebuffer into the background
+PID_XVFB="$!"      # take the process ID
+export DISPLAY=:99 # set display to use that of the xvfb
+
+# run the tests
+java -jar library/JsTestDriver-1.2.2.jar --config jsTestDriver.conf --port 4224 --browser $FIREFOX --tests all --testOutput $OUTPUT_DIR
+
+kill $PID_XVFB     # shut down xvfb (firefox will shut down cleanly by JsTestDriver)
+echo "Done."
