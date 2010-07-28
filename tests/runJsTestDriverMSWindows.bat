3c14146d35af33a57582ffe329dda605230f80f3
heyeung <heyeung@7ee224fa-57ca-4419-87f9-1f4314938d77>
2010-07-28 01:00:59 +0000

    Adding debugging information to see what is wrong.
    git-svn-id: https://develop.eng.uts.edu.au/projects/personal/sahara/svn/WI/trunk@1353 7ee224fa-57ca-4419-87f9-1f4314938d77

--- tests/runJsTestDriverMSWindows.bat
+++ tests/runJsTestDriverMSWindows.bat
@@ -1,2 +1,2 @@
 cd trunk\tests
-java -jar library\JsTestDriver-1.2.2.jar --port 4224 --browser "C:\Program Files\Internet Explorer\iexplore.exe" --tests all --testOutput .\coverage
+java -jar library\JsTestDriver-1.2.2.jar --port 4224 --browser "C:\Program Files\Internet Explorer\iexplore.exe" --tests all --testOutput .\coverage --runnerMode DEBUG
