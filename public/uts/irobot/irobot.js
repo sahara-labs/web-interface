09bbb5797c4bc1cbe31182430db927a8ecbf25c8
mdiponio <mdiponio@7ee224fa-57ca-4419-87f9-1f4314938d77>
2012-02-06 09:34:08 +0000

    Implmented ranger display.
    git-svn-id: https://develop.eng.uts.edu.au/projects/personal/sahara/svn/WI/trunk@3813 7ee224fa-57ca-4419-87f9-1f4314938d77

--- public/uts/irobot/irobot.js
+++ public/uts/irobot/irobot.js
@@ -69,6 +69,7 @@ IRobot.prototype.displayMode = function(mode) {
 	{
 	case 1: // Manual mode
 		this.widgets.push(new DPad(this));
+		this.widgets.push(new Ranger(this));
 		break;
 		
 	case 2: // Logging mode
@@ -172,10 +173,15 @@ IWidget.prototype.destroy = function () {
  * @param contents page contents
  */
 IWidget.prototype.pageAppend = function(contents) {
-	this.control.$canvas.append(
-			"<div id='" + this.wid + "' class='widget-box'>" + contents + "</div>"
-	);
+	var html = "<div id='" + this.wid + "' class='widget-box'>";
 	
+	if (this.title)
+	{
+		//html += "<div id='robot-widget-title>" + this.title + "</div>";
+	}
+	html += contents + "</div>";
+	
+	this.control.$canvas.append(html);
 	this.$w = this.control.$canvas.find("#" + this.wid);
 };
 
@@ -257,23 +263,27 @@ DPad.prototype.init = function() {
 	$(document).bind("keydown.dpad", function(evt) {
 		switch (evt.which)
 		{
+		case 38:  // Up arrow
 		case 87:  // W
 		case 119: // w
 			thiz.fingerPress("north");
 			break;
 			
+		case 39:  // Right arrow
 		case 68:  // D
 		case 100: // d
 			thiz.fingerPress("east");
 			break;
 			
+		case 40:  // Down arrow
 		case 83:  // S
-		case 115: //s
+		case 115: // s
 			thiz.fingerPress("south");
 			break;
 			
-		case 65:
-		case 97:
+		case 37:  // Left arrow
+		case 65:  // A
+		case 97:  // a
 			thiz.fingerPress("west");
 			break;
 		}
@@ -282,21 +292,25 @@ DPad.prototype.init = function() {
 	$(document).bind("keyup.dpad", function(evt) {
 		switch (evt.which)
 		{
+		case 38:  // Up arrow
 		case 87:  // W
 		case 119: // w
 			thiz.fingerRelease("north");
 			break;
 			
+		case 39:  // Right arrow
 		case 68:  // D
 		case 100: // d
 			thiz.fingerRelease("east");
 			break;
 			
+		case 40:  // Down arrow
 		case 83:  // S
 		case 115: // s
 			thiz.fingerRelease("south");
 			break;
 			
+		case 37:  // Left arrow
 		case 65:  // A
 		case 97:  // a
 			thiz.fingerRelease("west");
@@ -377,3 +391,268 @@ DPad.prototype.destroy = function () {
 	$(document).unbind("keydown.dpad keyup.dpad");
 	this.$w.remove();
 };
+
+/* ----------------------------------------------------------------------------
+ * -- Ranger display.                                                        --
+ * ---------------------------------------------------------------------------- */
+function Ranger(pc)
+{
+	IWidget.call(this, pc);
+	this.wid = "ranger-panel";
+	
+	this.title = "Ranging Laser";
+	
+	/* Ranger configuration. */
+	this.minRange = 0;
+	this.maxRange = 0;
+	this.rangerRes = 0;
+	this.minAngle = 0;
+	this.maxAngle = 0;
+	this.angularRes = 0;
+	
+	/* Canvas context. */
+	this.ctx;
+	this.width = this.height = 300;
+	this.xo = this.width / 2;
+	this.yo = this.height / 2;
+	this.pxPerM = this.width / 12;
+}
+Ranger.prototype = new IWidget;
+
+Ranger.prototype.init = function() {	
+	this.pageAppend(
+		"<canvas id='ranger' width='" + this.width + "' height='" + this.height + "'></canvas>"
+	);
+	
+	this.ctx = ($("#ranger")[0]).getContext("2d");
+	if (this.ctx)
+	{
+		this.getConf();
+	}
+	else
+	{
+		alert("Using this rig requires a modern browser!");
+	}
+};
+
+Ranger.prototype.getConf = function() {
+	var thiz = this;
+	$.get(
+		"/primitive/json/pc/" + IRobot.MANUAL_CONTROLLER + "/pa/rangerConf",
+		null,
+		function (resp) {
+			if (typeof resp != "object")
+			{
+				window.location.reload();
+				return;
+			}
+				
+			thiz.parseConf(resp);
+		}
+	);
+};
+
+Ranger.prototype.parseConf = function(conf) {
+	for (i in conf)
+	{
+		switch (conf[i].name)
+		{
+		case 'rangeres': 
+			this.rangerRes = parseFloat(conf[i].value);
+			break;
+		case 'minrange':
+			this.minRange = parseFloat(conf[i].value);
+			break;
+		case 'maxrange':
+			this.maxRange = parseFloat(conf[i].value);
+			break;
+		case 'minangle':
+			this.minAngle = Math.abs(parseFloat(conf[i].value));
+			break;
+		case 'maxangle':
+			this.maxAngle = parseFloat(conf[i].value);
+			break;
+		case 'angularres':
+			this.angularRes = parseFloat(conf[i].value);
+			break;
+		}
+	}
+	
+	if (this.minRange == 0)
+	{
+		/* Invalid data. */
+		var thiz = this;
+		setTimeout(function() {
+			thiz.getConf();
+		}, 1000);
+	}
+	else
+	{
+		this.mainLoop();
+	}
+};
+
+Ranger.prototype.mainLoop = function() {
+	var thiz = this;
+	$.get(
+		"/primitive/json/pc/" + IRobot.MANUAL_CONTROLLER + "/pa/ranger",
+		null,
+		function (resp) {
+			if (typeof resp != "object")
+			{
+				window.location.reload();
+				return;
+			}
+				
+			thiz.drawFrame(eval(resp.value));
+			
+			setTimeout(function() {
+				thiz.mainLoop();
+			}, 100);
+		}
+	);
+};
+
+Ranger.prototype.drawFrame = function(scan) {
+	
+	this.ctx.clearRect(0, 0, this.width, this.height);
+	
+	/* Grid lines and such. */
+	this.drawSkeleton();
+	
+	/* Ranger data. */
+	this.drawScan(scan);
+	
+	/* Draw puck. */
+//	this.drawPuck();
+};
+
+Ranger.prototype.drawSkeleton = function() {
+	var i;
+	
+	/* Grid lines. */
+	this.ctx.strokeStyle = "#DDDDDD";
+	this.ctx.lineWidth = 1.5;
+	this.ctx.moveTo(this.xo, 0);
+	this.ctx.lineTo(this.xo, this.height);
+	this.ctx.moveTo(0, this.yo);
+	this.ctx.lineTo(this.width, this.yo);
+	this.ctx.stroke();
+	
+	this.ctx.lineWidth = 0.75;
+	for (i = this.pxPerM; i < this.width; i += this.pxPerM)
+	{
+		/* Vertical. */
+		this.ctx.moveTo(i, 0);
+		this.ctx.lineTo(i, this.height);
+		
+		/* Horizontal. */
+		this.ctx.moveTo(0, i);
+		this.ctx.lineTo(this.width, i);
+	}
+	this.ctx.stroke();
+	
+	/* Draw blind region. */
+//	this.ctx.fillStyle = "rgba(200, 200, 200, 0.1)";
+//	this.ctx.beginPath();
+//	this.ctx.moveTo(this.xo, this.yo);
+//	this.ctx.lineTo(0, this.xo + this.width / 2 * Math.tan(Math.PI - this.minAngle));
+//	this.ctx.lineTo(0, this.xo - this.width / 2 * Math.tan(Math.PI - this.maxAngle));
+//	this.ctx.lineTo(this.xo, this.yo);
+//	this.ctx.closePath();
+//	this.ctx.fill();
+};
+
+Ranger.prototype.drawPuck = function() {
+	/* Robot puck at center. */
+	this.ctx.fillStyle = "#FFFFFF";
+	this.ctx.beginPath();
+	this.ctx.arc(150, 150, 9, 0, Math.PI * 2, true);
+	this.ctx.closePath();
+	this.ctx.fill();
+	
+	this.ctx.lineWidth = 2;
+	this.ctx.strokeStyle = "red";
+	this.ctx.beginPath();
+	this.ctx.arc(150, 150, 10, 0, Math.PI * 2, true);
+	this.ctx.closePath();
+	this.ctx.stroke();
+
+	this.ctx.fillStyle = "red";
+	this.ctx.beginPath();
+	this.ctx.moveTo(149, 150);
+	this.ctx.arc(150, 150, 10, Math.PI / 4, - Math.PI / 4, true);
+	this.ctx.moveTo(149, 150);
+	this.ctx.closePath();
+	this.ctx.fill();
+};
+
+Ranger.prototype.drawScan = function(scan){
+	var i = 0, s,
+		r = this.minAngle, 
+		radius = this.maxRange * this.pxPerM,
+		xd, yd;
+	
+	this.ctx.strokeStyle = "#BA0000";
+	this.ctx.lineWidth = 0.5;
+	this.ctx.fillStyle = "rgba(255, 40, 40, 0.75)";
+	this.ctx.beginPath();
+	this.ctx.moveTo(this.xo, this.yo);
+	
+	if (scan[0] == 0.0)
+	{
+		/* The range is infinite so a line is drawn to the start of the arc. */
+		this.ctx.lineTo(this.xo - radius * Math.sin(this.minAngle - Math.PI / 2), 
+						this.yo + radius * Math.cos(this.minAngle - Math.PI / 2));
+	}
+	
+	for (i = 0; i < scan.length; i++)
+	{		
+		if (scan[i] == 0.0)
+		{
+			/* Here the range is infinite so we will draw an arc, but we need 
+			 * to find the arc end which is up to range end. */
+			s = i;
+			while (++i < scan.length && scan[i] == 0.0);
+			i--;
+			this.ctx.arc(this.xo, this.yo, radius, r, r - (i - s) * this.angularRes, true);
+			
+			r -= (i - s) * this.angularRes;
+		}
+		else
+		{
+			xd = scan[i] * this.pxPerM * Math.sin(r - Math.PI / 2);
+			yd = scan[i] * this.pxPerM * Math.cos(r - Math.PI / 2);
+			
+			if (3 * Math.PI / 2 < r && r <= 2 * Math.PI)  // Top left quadrant
+			{
+				xd = this.xo + xd;
+				yd = this.yo - yd;
+			}
+			else if (Math.PI < r && r <= 3 * Math.PI / 2) // Top right quadrant
+			{
+				xd = this.xo - xd;
+				yd = this.yo - yd;
+			}
+			else if (Math.PI / 2 < r && r <= Math.PI)      // Bottom right quadrant
+			{
+				xd = this.xo - xd;
+				yd = this.yo + yd;
+			}
+			else if (r < Math.PI / 2)                      // Bottom left quadrant
+			{
+				xd = this.xo - xd;
+				yd = this.yo + yd;
+			}
+			
+			this.ctx.lineTo(xd, yd);
+			r -= this.angularRes;
+		}
+	}
+	
+	this.ctx.lineTo(this.xo, this.yo);
+	this.ctx.closePath();
+	this.ctx.stroke();
+	this.ctx.fill();
+};
+
