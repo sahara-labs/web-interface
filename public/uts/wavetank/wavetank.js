8f1d6cc7fa0bf3eeb282ac5825f75d569021986b
mdiponio <michael.diponio@uts.edu.au>
2012-11-13 21:04:04 +1100

    Started implemented wave tank dynamic behaviour.

--- public/uts/wavetank/wavetank.js
+++ public/uts/wavetank/wavetank.js
@@ -1,3 +1,49 @@
 /**
  * Wave Tank /basic/ user interface. 
  */
+
+/* Timestamp of last data update. */
+var dataUpdate = undefined;
+
+var isPumpOn = false;
+function setPump()
+{
+	if (!dataUpdate) return;
+	isPumpOn = !isPumpOn;
+	$("#controls0 img").attr("src", "/uts/wavetank/images/" + (isPumpOn ? "on" : "off" ) + ".png");
+	command("setPump", {on : isPumpOn});
+}
+
+var isInverterOn = false;
+function setInverter()
+{
+	if (!dataUpdate) return;
+	isInverterOn = !isInverterOn;
+	$("#controls1 img").attr("src", "/uts/wavetank/images/" + (isInverterOn ? "on" : "off") + ".png");
+	command("setInverter", {on: isInverterOn});
+}
+
+var paddleSpeed = 0;
+function setPaddle()
+{
+	
+}
+
+function setDigitalOut(num)
+{
+	alert("Set digital output " + num);
+}
+
+function setAnalogOutputs()
+{
+	alert("Set analog outputs");
+}
+
+function dataUpdate(data)
+{
+	
+}
+
+function command(action, params)
+{
+}
