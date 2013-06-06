4bb046e04d471cbaaad4dca839a054561c848fe0
jessecharlton <info@jessecharlton.com.au>
2013-06-06 11:56:18 +1000

    Created the Widget interface, DisplayManager and WaterLevelControl classes

--- public/uts/coupledtanksnew/coupledtanksnew.js
+++ public/uts/coupledtanksnew/coupledtanksnew.js
@@ -2,7 +2,95 @@
  * Coupled Tanks web interface.
  */
 
+/* ============================================================================
+ * == WaterLevelControl.                                                     ==
+ * ============================================================================ */
 
+function WaterLevelControl() { };
+
+/** Runs the Display Manager. */
+WaterLevelControl.prototype.setup = function() {
+	DisplayManager();
+};
+
+/** Retrieves latest data from the server. */
+WaterLevelControl.prototype.run = function() { };
+
+/* ============================================================================
+ * == Widget.                                                                ==
+ * ============================================================================ */
+
+function Widget() { };
+
+/** adds the widget to the page. */
+Widget.prototype.init = function() {
+    throw "Widget init not defined.";
+};
+
+/** Retrieves data from the server. */
+Widget.prototype.consume = function(data) { };
+
+/** Removes the widget from the page. */
+Widget.prototype.destroy = function() { };
+
+/** Updates the widget. */
+Widget.prototype.update = function(width,height) { };
+
+/** Adds a message to the page. */
+Widget.prototype.blur = function() { };
+
+/** Resets the widgets position. */
+Widget.prototype.requestData = function() { };
+
+/** Posts data to the server. */
+Widget.prototype.postControl = function(action,params) { };
+
+/* ============================================================================
+ * == Display Manager.                                                       ==
+ * ============================================================================ */
+
+function DisplayManager(control){
+	
+    Widget.call(this, control);
+    
+    this.PCONTROLLER = "CoupledTanksTwoController";
+	this.widgets = [];
+};
+
+DisplayManager.prototype = new Widget;
+DisplayManager.prototype.init = function() {
+	
+};
+
+/* ============================================================================
+ * == Page Widgets.                                                          ==
+ * ============================================================================ */
+
+function WaterLevelsMimic(control) {
+	
+	Widget.call(this, control);
+    this.width = 280;
+	this.height = 305;
+};
+
+WaterLevelsMimic.prototype.animateLoop = function() {
+	
+};
+
+function PIDControl(control) {
+	
+	Widget.call(this, control);
+    this.width = 255;
+	this.height = 210;
+};
+
+
+
+
+
+/* ============================================================================
+ * == Control.                                                               ==
+ * ============================================================================ */
 function CoupledTanks()
 {
 	
@@ -17,7 +105,7 @@ function CoupledTanks()
 	this.widgets = [];
 	
     
-	this.PCONTROLLER = "CoupledTanks";
+	this.PCONTROLLER = "CoupledTanksTwo";
 
 }
 
@@ -28,7 +116,7 @@ CoupledTanks.prototype.init = function() {
 CoupledTanks.prototype.requestData = function() {
 	var thiz = this;
 	$.ajax({
-		url: "/primitive/json/pc/CoupledTanks/pa/data",
+		url: "/primitive/mapjson/pc/" + PCONTROLLER + "/pa/data",
 		cache: false,
 		success: function(packet) {
 			if (typeof packet!= "object") 
@@ -62,8 +150,48 @@ CoupledTanks.prototype.isWorking = function() {
 	return this.working;
 };
 
+CoupledTanks.prototype.startSave = function() {};
+CoupledTanks.prototype.stopSave = function() {};
+
 /* ============================================================================
- * == Readings service.                                                      ==
+ * == Get Values.                                                            ==
+ * ============================================================================ */
+
+CoupledTanks.prototype.setPID = function(val) {
+	var thiz = this;
+	this.valve = val;
+	$.get("/primitive/mapjson/pc/" + this.PCONTROLLER + "/pa/setPID" + val,
+		   null,
+		   function(response) {
+				if (typeof response == 'object')
+				{
+					thiz.values(response);
+					thiz.repaint();
+				}
+				else thiz.raiseError("Failed response");
+			}
+	);
+};
+
+CoupledTanks.prototype.setValve = function(val) {
+	var thiz = this;
+	this.setpoint = val;
+	$.get("/primitive/mapjson/pc/" + this.PCONTROLLER + "/pa/setValve" + val,
+		   null,
+		   function(response) {
+				if (typeof response == 'object')
+				{
+					thiz.values(response);
+					thiz.repaint();
+				}
+				else thiz.raiseError("Failed response");
+			}
+	);
+};
+
+
+/* ============================================================================
+ * == Set Values.                                                            ==
  * ============================================================================ */
 
 CoupledTanks.prototype.values = function(values) {
@@ -71,50 +199,217 @@ CoupledTanks.prototype.values = function(values) {
 	{
 		switch (values[i].name)
 		{
-		case "setValve":
+		case "setPID":
 			this.valve = round(parseFloat(values[i].value, 10), 2);
 			break;
 			
-		case "setSetpoint":
+		case "setVavle":
 			this.setpoint = round(parseFloat(values[i].value, 10), 2);
 			break;
 		}
 	}
 };
+
 /* ============================================================================
- * == Rig control.                                                           ==
+ * == Slider                                                                 ==
  * ============================================================================ */
-CoupledTanks.prototype.setValve = function(val) {
-	var thiz = this;
-	this.valve = val;
-	$.get("/primitive/json/pc/" + this.PCONTROLLER + "/pa/setValve/pressure/" + val,
-		   null,
-		   function(response) {
-				if (typeof response == 'object')
-				{
-					thiz.values(response);
-					thiz.repaint();
-				}
-				else thiz.raiseError("Failed response");
-			}
-	);
+
+function Slider(){
+	
+	    $(".slider").slider({
+        range: "min",
+        min: 0,
+        max: 100,
+        value: 0,
+        slide: function(event, ui) {
+            $(".sliderValue").val(ui.value);
+            console.log('sliding');
+        }
+    });
+    
+    $(".sliderValue").change(function() {
+        var value = this.value.substring(1);
+        $(".slider").slider("value", parseInt(value));
+        $(".sliderValue").val($(".slider").slider("value"));
+    });
+};
+Slider.prototype.inti = function() {};
+Slider.prototype.slide = function() {};
+Slider.prototype.update = function() {};
+
+/* ============================================================================
+ * == Page Elements.                                                         ==
+ * ============================================================================ */
+    
+    /* JQuery Tabs. */
+    $("#tabs").tabs();
+    $("#diagramTabs").tabs();
+    $(".windowcontent").resizable();
+    $(".resizableVideo").resizable({
+        aspectRatio: 16 / 9,
+        minHeight: 192,
+        minWidth: 108
+    });
+    
+    /* Toggle Buttons. */
+    $('.toggle').click(function() {
+        var x = '.' + $(this).attr('name');
+        var y = $(this);
+        $(x).is(':visible') ? $(x).hide('fade', 150) : $(x).show('fade', 150);
+        if ($(this).find('.switch').find('.slide').hasClass('off')) {
+            $(this).find('.switch').find('.slide').addClass("on").removeClass("off");
+        }else{
+            $(this).find('.switch').find('.slide').addClass("off").removeClass("on");
+        } 
+    }); 
+    
+    function randomNum(){	
+        var x = Math.floor(Math.random()*100)+1;
+        return x;
+    };
+        
+    $.ui.plugin.add('draggable', 'increaseZindexOnmousedown', {
+        create: function() {
+            this.mousedown(function(e) {
+                var inst = $(this).data('draggable');
+                inst._mouseStart(e);
+                inst._trigger('start', e);
+                inst._clear();
+            });
+        }
+    });
+    
+    $(".windowwrapper").draggable({
+        snap: true,
+        snapTolerance: 5,
+        stack: '.windowwrapper',
+        increaseZindexOnmousedown: true
+    });
+
+/* ============================================================================
+ * == Page Widgets.                                                          ==
+ * ============================================================================ */
+
+function ControlPanel() {};
+ControlPanel.prototype.init = function() {};
+ControlPanel.prototype.setManualMode = function() {};
+ControlPanel.prototype.toggle = function() {};
+
+function diagram(CTinst)
+{
+	CoupledTanksWidget.call(this, CTinst);
+	
+	/* Default diagram properties. */
+	this.width = 320;
+	this.height = 240;
+    this.duration = "";
+    this.period = "";
+    this.l1 = "";
+    this.l2 = "";
+    this.t1In = "";
+    this.t2Out = "";
+    this.t1Tot2 = "";
+}
+
+diagram.prototype.init = function() {};
+
+diagram.prototype.animateTanks = function() {
+
+	/* Sets tank variables. */
+    var valvepercent;
+    var pumprpm;
+    var tankoneflowin;
+    var flowsensorbetween;
+    var tanktwoflowout;
+    var levelBottom;
+    
+    
+    $('.toggleWater').click(function(){ 
+    	startSpin()
+    	var percent =  "%";
+        var tubeOneHeight;
+        var i = 0; while (i<6){
+        tubeOneHeight = randomNum();
+        tubeTwoHeight = randomNum();
+        tubeThreeHeight = randomNum();
+    
+        /* Gets latest data. */
+        valvepercent = randomNum();
+        pumprpm = randomNum();
+        console.log('');
+        tankoneflowin = randomNum();
+        flowsensorbetween = randomNum();
+        tanktwoflowout = randomNum();
+        levelBottom = randomNum();
+    
+        /*Animates the water levels */
+        $('.tubeOne').animate({"height": tubeOneHeight+percent }, 400);
+        $('.tubeTwo').animate({"height": tubeTwoHeight+percent }, 400);
+        $('.tubeThree').animate({"height": tubeTwoHeight+percent }, 400);
+        
+        /* Displays the latest data */
+        $(".valvepercent").val(valvepercent + percent);
+        $(".pumprpm").val(pumprpm + " RPM");
+        $(".tankoneflowin").val(tankoneflowin + " L/m");
+        $(".flowsensorbetween").val(flowsensorbetween + " L/m");
+        $(".tanktwoflowout").val(tanktwoflowout + " L/m");
+        $(".levelsensorone").val(tubeOneHeight);
+        $(".levelsensortwo").val(tubeTwoHeight);
+
+        i++;
+        };
+    });	
 };
 
-CoupledTanks.prototype.setSetpoint = function(val) {
-	var thiz = this;
-	this.setpoint = val;
-	$.get("/primitive/json/pc/" + this.PCONTROLLER + "/pa/setSetpoint/pressure/" + val,
-		   null,
-		   function(response) {
-				if (typeof response == 'object')
-				{
-					thiz.values(response);
-					thiz.repaint();
-				}
-				else thiz.raiseError("Failed response");
-			}
-	);
+diagram.prototype.animateSpin = function() {
+    var angle = 0;
+    setInterval(function(){
+        angle+=3;
+        $(".spinner").rotate(angle);
+    },10); 
 };
+
+diagram.prototype.toggle = function() {};
+
+function camera(CTinst)
+{
+	CoupledTanksWidget.call(this, CTinst);
+	
+	/* Default diagram properties. */
+	this.width = 320;
+	this.height = 240;
+}
+diagram.prototype.init = function() {};
+diagram.prototype.resize = function() {};
+diagram.prototype.pausefeed = function() {};
+diagram.prototype.deployVideo = function() {};
+diagram.prototype.deploySWF = function() {};
+diagram.prototype.deployMPEG = function() {};
+diagram.prototype.Format = function() {};
+diagram.prototype.toggle = function() {};
+
+function toggle(CTinst){
+	CoupledTanksWidget.call(this, CTinst);
+	
+	/* Default diagram properties. */
+	this.width = 320;
+	this.height = 240;
+}
+toggle.prototype.init = function(){};
+toggle.prototype.toggleWidget = function(){};
+
+function chart(CTinst){
+	CoupledTanksWidget.call(this, CTinst);
+	
+	/* Default diagram properties. */
+    this.width = "";
+    this.height = "";
+}
+chart.prototype.init = function() {};
+chart.prototype.repaint = function() {};
+chart.prototype.clearPlot = function() {};
+chart.prototype.toggle = function() {};
+
 
 /* ============================================================================
  * == Utility & debug.                                                       ==
@@ -142,169 +437,3 @@ CoupledTanks.prototype.raiseError = function(error, level) {
 		console.error("CoupledTanks Err: " + error);		
 	}
 };
-
-
-function getCanvas(id, width, height)
-{
-	var canvas = document.createElement("canvas");
-	canvas.setAttribute("id", id);
-	canvas.setAttribute("width", width);
-	canvas.setAttribute("height", height);
-	
-	if (typeof G_vmlCanvasManager != "undefined")
-	{
-		/* Hack to get canvas setup on IE6 to 8 which don't support canvas
-		 * natively. */
-		G_vmlCanvasManager.initElement(canvas);
-	}
-	
-	return canvas;
-}
-/* ============================================================================
- * == Page widgets.                                                          ==
- * ============================================================================ */
-
-CoupledTanks.prototype.Widget = function(wid,icon,title,content) {
-	
-	this.wid = wid;
-	this.icon = icon;
-	this.title = title;
-	this.content = content;
-	
-	$("body").append(
-			"<div class='windowwrapper' id=" + this.wid + ">" +
-			    "<div class='windowheader'>" + this.icon +
-			        "<span class='windowtitle'>" + this.title +
-			        "</span>" +
-			    "</div>" +
-			    
-			    "<div class='windowcontent'>" + this.content +
-			    "</div>" +
-			"</div>"
-	);
-};
-
-    $.ui.plugin.add('draggable', 'increaseZindexOnmousedown', {
-        create: function() {
-            this.mousedown(function(e) {
-                var inst = $(this).data('draggable');
-                inst._mouseStart(e);
-                inst._trigger('start', e);
-                inst._clear();
-            });
-        }
-    });
-    
-    $(".windowwrapper").draggable({
-        snap: true,
-        snapTolerance: 5,
-        stack: '.windowwrapper',
-        increaseZindexOnmousedown: true
-    });
-    
-	/* JQuery Tabs. */
-    $("#tabs").tabs();
-    $("#diagramTabs").tabs();
-    $(".windowcontent").resizable();
-    $(".resizableVideo").resizable({
-        aspectRatio: 16 / 9,
-        minHeight: 192,
-        minWidth: 108
-    });
-    
-	/* Toggle Buttons. */
-    $('.toggle').click(function() {
-        var x = '.' + $(this).attr('name');
-        var y = $(this);
-        $(x).is(':visible') ? $(x).hide('fade', 150) : $(x).show('fade', 150);
-        if ($(this).find('.switch').find('.slide').hasClass('off')) {
-            $(this).find('.switch').find('.slide').addClass("on").removeClass("off");
-        }else{
-            $(this).find('.switch').find('.slide').addClass("off").removeClass("on");
-        } 
-    });  
-    
-/* ============================================================================
- * == Diagram                                                                ==
- * ============================================================================ */    
-    
-CoupledTanks.prototype.diagram = function(){
-    
-    var valvepercent;
-    var pumprpm;
-    var tankoneflowin;
-    var flowsensorbetween;
-    var tanktwoflowout;
-    var levelBottom;
-    
-    $('.toggleWater').click(function(){ 
-    	startSpin()
-    	var percent =  "%";
-        var tubeOneHeight;
-        var i = 0; while (i<6){
-        tubeOneHeight = randomNum();
-        tubeTwoHeight = randomNum();
-        tubeThreeHeight = randomNum();
-
-    valvepercent = randomNum();
-    pumprpm = randomNum();
-    console.log('');
-    tankoneflowin = randomNum();
-    flowsensorbetween = randomNum();
-    tanktwoflowout = randomNum();
-    levelBottom = randomNum();
-    
-        $('.tubeOne').animate({"height": tubeOneHeight+percent }, 400);
-        $('.tubeTwo').animate({"height": tubeTwoHeight+percent }, 400);
-        $('.tubeThree').animate({"height": tubeTwoHeight+percent }, 400);
-        
-        $(".valvepercent").val(valvepercent + percent);
-        $(".pumprpm").val(pumprpm + " RPM");
-        $(".tankoneflowin").val(tankoneflowin + " L/m");
-        $(".flowsensorbetween").val(flowsensorbetween + " L/m");
-        $(".tanktwoflowout").val(tanktwoflowout + " L/m");
-        $(".levelsensorone").val(tubeOneHeight);
-        $(".levelsensortwo").val(tubeTwoHeight);
-
-        i++;
-        };
-    });
-};
-    
-    function startSpin(){
-        var angle = 0;
-        setInterval(function(){
-            angle+=3;
-            $(".spinner").rotate(angle);
-        },10);
-    };
-    function randomNum(){	
-        var x = Math.floor(Math.random()*100)+1;
-        return x;
-    };
-      
-/* ============================================================================
- * == Slider                                                                ==
- * ============================================================================ */
-CoupledTanks.prototype.slider = function(){
-    
-    $(".slider").slider({
-        range: "min",
-        min: 0,
-        max: 100,
-        value: 0,
-        slide: function(event, ui) {
-            $(".sliderValue").val(ui.value);
-            console.log('sliding');
-        }
-    });
-    
-    $(".sliderValue").change(function() {
-        var value = this.value.substring(1);
-        $(".slider").slider("value", parseInt(value));
-        $(".sliderValue").val($(".slider").slider("value"));
-    });
-};
-/* ============================================================================
- * == Camera Display.                                                        ==
- * ============================================================================ */
