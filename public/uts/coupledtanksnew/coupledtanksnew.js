dcaf44ae9c2252ec56567fa9a6b17ecae53b8d32
Michael Diponio <michael.diponio@uts.edu.au>
2013-06-28 16:38:48 +1000

    Restored data variable toggle to working.

--- public/uts/coupledtanksnew/coupledtanksnew.js
+++ public/uts/coupledtanksnew/coupledtanksnew.js
@@ -665,10 +665,11 @@ GraphWidget.prototype.init = function() {
 	
 	/* Event handlers. */
 	var thiz = this;
-	this.$widget.find(".graph-label-enable").click(function() {
-		thiz.showTrace($(this).next().text(), $(this).find('div').hasClass(".on"));
+	this.$widget.find('.graph-label').click(function() {    
+		thiz.showTrace($(this).children(".graph-label-text").text(), 
+				$(this).find(".switch .slide").toggleClass("on off").hasClass("on"));
 	});
-	
+
 	/* Draw the first frame contents. */
 	this.drawFrame();
 	
@@ -676,14 +677,6 @@ GraphWidget.prototype.init = function() {
 	if (this.isPulling) this.acquireData();
 	
 	this.enableDraggable();
-
-	/** Align the boxes for webkit browers*/
-
-   
-    
-   this.$widget.find('.graph-label').click(function() {    
-       $(this).find('.switch .slide').toggleClass("on off");
-   });
 };
 
 /** The number of vertical scales. */
